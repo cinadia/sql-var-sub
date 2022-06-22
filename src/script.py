3ac00c83a3a34907ba5f5e06f69d50027559c55f
cinadia <73308097+cinadia@users.noreply.github.com>
2022-06-22 15:42:46 -0700

    Create SQL line-by-line parser

--- src/script.py
+++ src/script.py
@@ -1,19 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
-import psycopg2
-# or from sqlalchemy.sql import text
 
 ENGINE_URL = 'postgresql://postgres:postgres@localhost:5129/postgres'
 
 engine = create_engine(ENGINE_URL)  # sqlalchemy engine
-
-# conn = psycopg2.connect( # psycopg2 connection
-#     host="localhost",
-#     database="postgres",
-#     user="postgres",
-#     password="ccuipostgresql",
-#     port="5129")
-# conn.set_session(autocommit=False)
 
 with engine.connect() as con:
     with open('substitution_builder.sql') as file:
--- src/test_query.sql
+++ src/test_query.sql
@@ -1,1 +1,9 @@
+SELECT * FROM boc_rates;
 
+
+SELECT * FROM boc_rates
+    WHERE prime_rate >= 11
+    ORDER BY prime_rate DESC;
+
+-- DELETE FROM second_boc_rates
+--        WHERE prime_rate >= 11;
